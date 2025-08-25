7a809adda30618e6cd7c71e1aadccc15b81ebd92
mhy999999 <165057032+mhy999999@users.noreply.github.com>
2025-08-25 11:41:00 +0800

    修改部分

--- AIPartnerPYTHON/app/models/TestAI.py
+++ AIPartnerPYTHON/app/models/TestAI.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel
 
 
 class TestAI(BaseModel):
-    description: str  # 内容描述
+    display_name: str  # 内容描述
     difficulty: int  # 难度等级
-    display_name: str  # 显示名称
-    nmb: list[int]  # 编号序列
-    subject: str  # 学科名称
-    type: list[str]  # 试题类型
+    subject: str  # 显示名称
+    difficulty: str  # 编号序列
+    response: str  # 学科名称
+    TorF: str  # 试题类型
+    choice:str
 
 
 class TestAIWorkflowRun(BaseModel):
